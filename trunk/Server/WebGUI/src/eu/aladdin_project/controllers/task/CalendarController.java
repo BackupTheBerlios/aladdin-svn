ed32090c8cfd0204cb69c53c921c623a689a4d2a
fdandria <fdandria@459d45aa-ea84-0410-b58b-f86611838273>
2010-09-15 12:43:50 +0000

    Dynamic task retrieve using calendar interface(2)

--- trunk/Server/WebGUI/src/eu/aladdin_project/controllers/task/CalendarController.java
+++ trunk/Server/WebGUI/src/eu/aladdin_project/controllers/task/CalendarController.java
@@ -17,7 +17,7 @@ public class CalendarController extends GenericForwardComposer {
 	public Window bookEventWin;
 	public Calendars cal;
 	
-	public void onEventCreate$cal(CalendarsEvent event) throws InterruptedException{
+	public void onEventCreate(CalendarsEvent event) throws InterruptedException{
 		bookEventWin = (Window)Executions.createComponents("bookEvent.zul", self.getParent(), null);
 		bookEventWin.setAttribute("calendars", cal);
 		bookEventWin.setAttribute("calevent", event);
@@ -25,6 +25,23 @@ public class CalendarController extends GenericForwardComposer {
 		setting.setTime(setting.getTime()+43200000);
 		((Datebox)bookEventWin.getFellow("datetask")).setValue(setting);
 		((Timebox)bookEventWin.getFellow("timetask")).setValue(setting);
+		bookEventWin.setVisible(true);
+		bookEventWin.doModal();
+	}
+	
+	public void onEventEdit(CalendarsEvent event) throws InterruptedException{
+		System.out.println("AAAAA");
+		bookEventWin = (Window)Executions.createComponents("bookEvent.zul", self.getParent(), null);
+		bookEventWin.setAttribute("calendars", cal);
+		bookEventWin.setAttribute("calevent", event);
+		System.out.println("BBBBB");
+		Date setting = event.getCalendarEvent().getBeginDate();
+		System.out.println("CCCCC");
+		setting.setTime(setting.getTime()+43200000);
+		System.out.println("CCCCC");
+		((Datebox)bookEventWin.getFellow("datetask")).setValue(setting);
+		((Timebox)bookEventWin.getFellow("timetask")).setValue(setting);
+		System.out.println("DDDDD");
 		bookEventWin.setVisible(true);
 		bookEventWin.doModal();
 	}
