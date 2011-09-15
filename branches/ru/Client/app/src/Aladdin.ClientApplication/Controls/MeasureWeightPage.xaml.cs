cdbbbe2bff12a6a5389de4379525d966e0f71801
ababoshin <ababoshin@459d45aa-ea84-0410-b58b-f86611838273>
2011-09-15 12:41:48 +0000

    sync

---
+++ branches/ru/Client/app/src/Aladdin.ClientApplication/Controls/MeasureWeightPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Aladdin.ClientApplication.Windows;
+using Aladdin.ClientApplication.ViewModels;
+
+namespace Aladdin.ClientApplication.Controls
+{
+    /// <summary>
+    /// Interaction logic for MeasureWeightBloodPage.xaml
+    /// </summary>
+    public partial class MeasureWeightPage : UserControl
+    {
+        MeasureWeightViewModel ViewModel { get; set; }
+
+        public MeasureWeightPage(aladdinService.Task activeTask)
+        {
+            this.ViewModel = new MeasureWeightViewModel(activeTask);
+            this.DataContext = this.ViewModel;
+
+            InitializeComponent();
+            //if (MainWindow.Questionnaire != null)
+            //    this.ChooseOneAnswerFromManyControl.FillAnswers(MainWindow.Questionnaire.Questions[4].Questions[0]);
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            e.Handled = true;
+            if (AppCommands.SendMeasurementCommand.Equals(e.Command))
+                this.ViewModel.SendMeasurements();
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.Handled = true;
+            e.CanExecute = this.ViewModel.CanSendMeasurements();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            NumberPad pad = new NumberPad();
+            pad.DisplayText = this.ViewModel.WeightText;
+            pad.NumberPadPressed += new NumberPadPressedEventHandler(pad_NumberPadPressed);
+            pad.NumberPadClosed += new NumberPadClosedEventHandler(pad_NumberPadClosed);
+            pad.ShowDialog();
+        }
+
+        void pad_NumberPadClosed(object sender)
+        {
+            this.NoButton.Focus();
+        }
+
+        void pad_NumberPadPressed(object sender, string character)
+        {
+            bool insert = true;
+            if (character.Equals("BACKSPACE"))
+            {
+                if (this.ViewModel.WeightText.Length > 0)
+                {
+                    this.ViewModel.WeightText = this.ViewModel.WeightText.Substring(0, this.ViewModel.WeightText.Length - 1);
+                }
+                return;
+            }
+            else if (character.Equals(System.Threading.Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator))
+                if (this.ViewModel.WeightText.Contains(character))
+                    insert = false;
+            if (insert)
+            {
+                if (character.Equals(System.Threading.Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator))
+                    this.ViewModel.WeightText += System.Threading.Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+                else
+                    this.ViewModel.WeightText += character;
+            }
+
+            
+        }
+    }
+}
---
+++ branches/ru/Client/app/src/Aladdin.ClientApplication/Controls/MediaContentSectionPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aladdin.ClientApplication.Controls
+{
+    /// <summary>
+    /// Interaction logic for MediaContentSectionPage.xaml
+    /// </summary>
+    public partial class MediaContentSectionPage : UserControl
+    {
+        public MediaContentSectionPage(string category)
+        {
+            InitializeComponent();
+            this.Category = category;
+        }
+
+        public string Category { get; set; }
+
+        List<aladdinService.MediaContent> SectionMediaContent = new List<aladdinService.MediaContent>();
+
+        private void LoadContent()
+        {
+            Cursor = Cursors.Wait;
+            this.MediaContentPanel.Children.Clear();
+
+            switch (Category)
+            {
+                case "entertainment":
+                    SectionMediaContent = App.EntertainmentContent.ToList();
+                    break;
+                case "games":
+                    SectionMediaContent = App.GamesContent.ToList();
+                    break;
+                case "education":
+                    SectionMediaContent = App.EducationContent.ToList();
+                    break;
+                default:
+                    break;
+            }
+
+            foreach (aladdinService.MediaContent content in SectionMediaContent)
+            {
+                MediaContentControl mediaContentControl = new MediaContentControl();
+                mediaContentControl.Title = content.title;
+                mediaContentControl.MediaContent = content;
+                this.MediaContentPanel.Children.Add(mediaContentControl);
+            }
+
+            Cursor = Cursors.Arrow;
+        }
+
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadContent();
+        }
+
+    }
+}
